a7f7c47f1f6f73e0bfa3ad0525e2963961d77a54
daniel-ellis <daniel.ellis@bbc.co.uk>
2024-09-26 14:18:34 +0100

    Initial Commit

---
+++ HashTableUnitTests/HashTableUnitTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HashTableUnitTestsApp.swift
+//  HashTableUnitTests
+//
+//  Created by Dan Ellis on 26/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct HashTableUnitTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
